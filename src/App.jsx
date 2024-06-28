e8ec900f4ddc4bd1c362c664f2e02caf86087235
D0minnn <bendg108@gmail.com>
2024-06-28 19:36:14 +0800

    fixed the bg

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,17 @@ import Technologies from "./components/Technologies"
 const App = () => {
   return (
     <div className="overflow-x-hidden antialiased selection:bg-green-100 selection:text-gray-500">
-      <div className="flixed top-0 -z-10 h-full w-full">
+      <div className="fixed top-0 -z-10 h-full w-full">
 
       </div>
-      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
+      <div className="fixed inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
       
       <div className="container mx-auto px-8">
         <Navbar />  
         <Hero />
         <About />
         <Technologies />
+        
       </div>
     </div>
   )
